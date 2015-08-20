2c853cb1d6d613a8c8c3079e4e26f223b3c2e5ba
Arthur <tiancaiamao@gmail.com>
2015-08-20 15:13:24 +0800

    添加函数uncover-locals，去掉let表，提取局部变量

--- cc.scm
+++ cc.scm
@@ -3,26 +3,26 @@
 (define trivial? (lambda (x) (memq x '(zero? add1 sub1 + *))))
 
 (define reduce
-  (lambda (f ls rv)    
+  (lambda (f ls rv)
     (if (null? ls)
-	rv
-	(reduce f (cdr ls) (f (car ls) rv)))))
+        rv
+        (reduce f (cdr ls) (f (car ls) rv)))))
 
 (define filter
   (lambda (f ls)
     (cond
      ((null? ls) '())
      ((f (car ls)) (filter f (cdr ls)))
-     (else 
+     (else
       (cons (car ls) (filter f (cdr ls)))))))
 
 (define (remove x ls)
   (filter (lambda (x0) (eq? x0 x)) ls))
 
 (define (difference lst1 lst2)
-    (if (not (pair? lst2))
-	lst1
-	(difference (remove (car lst2) lst1) (cdr lst2))))
+  (if (not (pair? lst2))
+      lst1
+      (difference (remove (car lst2) lst1) (cdr lst2))))
 
 (define (insert x ls)
   (cons x (remove x ls)))
@@ -30,9 +30,9 @@
 (define union
   (lambda (lst1 lst2)
     (if (null? lst1)
-	lst2
-	(insert (car lst1)
-		(union (cdr lst1) lst2)))))
+        lst2
+        (insert (car lst1)
+                (union (cdr lst1) lst2)))))
 
 (define environments '())
 (define num-envs 0)
@@ -48,19 +48,19 @@
 (define free-vars
   (lambda (exp)
     (match exp
-	   ((? const?) '())
-	   ((? symbol?) (list exp))
-	   (('lambda (vars ...) body ...)
-	    (difference (free-vars body) vars))
-	   (('if a b c)
-	    (union (free-vars a)
-		   (union (free-vars b)
-			  (free-vars c))))
-	   (('set! var val)
-	    (union var (free-vars val)))
-	   (('begin es ...) (reduce union (map free-vars es) '()))
-	   ((f es ...) (reduce union (map free-vars exp) '()))
-	   (else (error "unknown expression:" exp)))))
+           ((? const?) '())
+           ((? symbol?) (list exp))
+           (('lambda (vars ...) body ...)
+            (difference (free-vars body) vars))
+           (('if a b c)
+            (union (free-vars a)
+                   (union (free-vars b)
+                          (free-vars c))))
+           (('set! var val)
+            (union var (free-vars val)))
+           (('begin es ...) (reduce union (map free-vars es) '()))
+           ((f es ...) (reduce union (map free-vars exp) '()))
+           (else (error "unknown expression:" exp)))))
 
 (define substitute-var
   (lambda (env x)
@@ -74,36 +74,36 @@
      ((eq? (caar env) x) 
       (assq-remove-key x (cdr env)))
      (else (cons (car env)
-		 (assq-remove-key x (cdr env)))))))
+                 (assq-remove-key x (cdr env)))))))
 
 (define assq-remove-keys
   (lambda (lst env)
     (if (null? lst)
-	env
-	(assq-remove-keys (cdr lst)
-			  (assq-remove-key (car lst) env)))))
+        env
+        (assq-remove-keys (cdr lst)
+                          (assq-remove-key (car lst) env)))))
 
 (define substitute
   (lambda (env exp)
     (define substitute-with (lambda (e) (substitute env e)))
     (match exp
-	   ((? const?) exp)
-	   ((? symbol?)
-	    (substitute-var env exp))
-	   (('if a b c)
-	    `(if ,(substitute env a)
-		 ,(substitute env b)
-		 ,(substitute env c)))
-	   (('begin es ...)
-	    (cons 'begin (map substitute-with es)))
-	   (('$closure . _) exp)
-					;     ((set!? exp)
-					;     `(set! ,(substitute-var env (set!->var exp))
-					;	     ,(substitute-var env (set!->val exp))))
-	   (('lambda (var ...) body ...)
-	    `(lambda ,var
-	       ,(substitute (assq-remove-keys var env) body)))
-	   ((f es ...) (map substitute-with exp)))))
+           ((? const?) exp)
+           ((? symbol?)
+            (substitute-var env exp))
+           (('if a b c)
+            `(if ,(substitute env a)
+                 ,(substitute env b)
+                 ,(substitute env c)))
+           (('begin es ...)
+            (cons 'begin (map substitute-with es)))
+           (('$closure . _) exp)
+                                        ;     ((set!? exp)
+                                        ;     `(set! ,(substitute-var env (set!->var exp))
+                                        ;	     ,(substitute-var env (set!->val exp))))
+           (('lambda (var ...) body ...)
+            `(lambda ,var
+               ,(substitute (assq-remove-keys var env) body)))
+           ((f es ...) (map substitute-with exp)))))
 
 (define (azip list1 list2)
   (if (and (pair? list1) (pair? list2))
@@ -115,34 +115,34 @@
 (define fv->sub
   (lambda (fv idx env)
     (if (null? fv)
-	'()
-	(cons (cons (car fv) `(env-get ,idx ,env))
-	      (fv->sub (cdr fv) (+ idx 1) env)))))
+        '()
+        (cons (cons (car fv) `(env-get ,idx ,env))
+              (fv->sub (cdr fv) (+ idx 1) env)))))
 
 (define closure-convert
   (lambda (exp)
     (match exp
-	   ((? const?) exp)
-	   ((? symbol?) exp)
-	   (('lambda (vars ...) body ...)
-	    (let* ((body1 (closure-convert body))
-		   (fv (difference (free-vars body) vars))
-		   (id (allocate-environment fv))
-		   ($env (gensym 'env))
-		   (sub (fv->sub fv 0 $env)))
-	      `($closure (lambda (,$env ,@vars)
-			   ,(substitute sub body1))
-			 (env-make ,(length fv) ,@fv))))
-	   (('if a b c)
-	    `(if ,(closure-convert a)
-		 ,(closure-convert b)
-		 ,(closure-convert c)))
-					;     ((set!? exp)
-					;      `(set! ,(set!->var exp)
-					;	     ,(closure-convert (set!->val exp))))
-					;     ((define? exp)
-					;      `(define ,(define->var exp)
-					;	 ,(closure-convert (define->val exp))))
-	   ((f es ...)
-	    (map closure-convert exp))
-	   (else (error "unhandled exp: " exp)))))
+           ((? const?) exp)
+           ((? symbol?) exp)
+           (('lambda (vars ...) body ...)
+            (let* ((body1 (closure-convert body))
+                   (fv (difference (free-vars body) vars))
+                   (id (allocate-environment fv))
+                   ($env (gensym 'env))
+                   (sub (fv->sub fv 0 $env)))
+              `($closure (lambda (,$env ,@vars)
+                           ,(substitute sub body1))
+                         (env-make ,(length fv) ,@fv))))
+           (('if a b c)
+            `(if ,(closure-convert a)
+                 ,(closure-convert b)
+                 ,(closure-convert c)))
+                                        ;     ((set!? exp)
+                                        ;      `(set! ,(set!->var exp)
+                                        ;	     ,(closure-convert (set!->val exp))))
+                                        ;     ((define? exp)
+                                        ;      `(define ,(define->var exp)
+                                        ;	 ,(closure-convert (define->val exp))))
+           ((f es ...)
+            (map closure-convert exp))
+           (else (error "unhandled exp: " exp)))))
