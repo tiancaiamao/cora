272b0cac665fa7ebe35e81d737d3e6b984bd41d2
Arthur <tiancaiamao@gmail.com>
2015-08-30 10:35:16 +0800

    添加main.scm，编译器的基本架构

---
+++ main.scm
@@ -0,0 +1,36 @@
+;; ---------utils-----------
+(load "match.scm")
+(load "helpers.scm")
+;; --------pass combinator------
+(load "nanopass.scm")
+;; --------passes-----------
+(load "generate-x86-64.scm")
+(load "flatten-program.scm")
+(load "expose-frame-var.scm")
+(load "expose-basic-blocks.scm")
+(load "finalize-locations.scm")
+(load "assign-registers.scm")
+(load "liveness-analysis.scm")
+
+(load "convert-closures.scm")
+
+(load "cps-convert.scm")
+(load "canonicalize-expression.scm")
+
+(define compile
+  (nanopass
+   expand-syntax
+   canonicalize-expression
+
+   cps-convert
+
+   convert-closures
+
+   liveness-analysis
+   assign-registers
+   finalize-locations
+   expose-frame-var
+   expose-basic-blocks
+   flatten-program
+   generate-x86-64
+   ))
