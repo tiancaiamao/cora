b0df07b7e36334c4f5f4bcf530080c7f23621592
Arthur <tiancaiamao@gmail.com>
2015-09-11 01:58:52 +0800

    fix build of main.scm using chez petite

--- main.scm
+++ main.scm
@@ -1,52 +1,55 @@
 ;; ---------utils-----------
-(load "match.scm")
-(load "helpers.scm")
+(load "match.ss")
+(load "helpers.ss")
 ;; --------pass combinator------
 (load "nanopass.scm")
 ;; --------passes-----------
-(load "generate-x86-64.scm")
-(load "flatten-program.scm")
-(load "expose-frame-var.scm")
-(load "expose-basic-blocks.scm")
-(load "finalize-locations.scm")
-(load "liveness-analysis.scm")
-(load "assign-registers.scm")
-(load "convert-closures.scm")
-(load "canonicalize-expression.scm")
-(load "remove-complex-opera.scm")
-(load "impose-calling-conventions.scm")
+(load "parse-scheme.scm")
+(load "convert-complex-datum.scm")
+;(load "generate-x86-64.scm")
+;(load "flatten-program.scm")
+;(load "expose-frame-var.scm")
+;(load "expose-basic-blocks.scm")
+;(load "finalize-locations.scm")
+;(load "liveness-analysis.scm")
+;(load "assign-registers.scm")
+;(load "convert-closures.scm")
+;(load "canonicalize-expression.scm")
+;(load "remove-complex-opera.scm")
+;(load "impose-calling-conventions.scm")
 
 (define compile
   (nanopass
-
    parse-scheme
    convert-complex-datum
-   uncover-assigned
-   purify-letrec
-   convert-assignments
-   remove-anonymous-lambda
-   sanitize-binding-forms
-   uncover-free
-   convert-closures
-   introduce-procedure-primitives
-   lift-letrec
-   normalize-context
-   specify-representation
-   uncover-locals
-   remove-let
-   verify-uil
+
+   ;;   uncover-assigned
+   ;;   purify-letrec
+   ;;   convert-assignments
+                                        ;   remove-anonymous-lambda
+                                        ;   sanitize-binding-forms
+                                        ;   uncover-free
+                                        ;   convert-closures
+                                        ;   introduce-procedure-primitives
+                                        ;   lift-letrec
+                                        ;   normalize-context
+                                        ;   specify-representation
+                                        ;   uncover-locals
+                                        ;   remove-let
+                                        ;   verify-uil
 
 
-;;   expand-syntax
-;;   canonicalize-expression
-;;   convert-closures
+   ;;   expand-syntax
+   ;;   canonicalize-expression
+   ;;   convert-closures
 
-   remove-complex-opera
-   flatten-set!
-   impose-calling-conventions
+                                        ;   remove-complex-opera
+                                        ;   flatten-set!
+                                        ;   impose-calling-conventions
 
-;;   uncover-frame-conflict
+   ;;   uncover-frame-conflict
 
+   #|
    liveness-analysis
    assign-registers
    finalize-locations
@@ -54,4 +57,5 @@
    expose-basic-blocks
    flatten-program
    generate-x86-64
+   |#
    ))
