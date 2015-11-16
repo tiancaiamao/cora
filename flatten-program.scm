05c9898a426c9c1d90819ded221b32bfdaad298c
Arthur <tiancaiamao@gmail.com>
2015-11-16 22:04:33 +0800

    change flatten-program to accept the new input format

--- flatten-program.scm
+++ flatten-program.scm
@@ -1,102 +1,53 @@
 #|
-(letrec ([f$1 (lambda ()
-                (begin
-                  (set! <disp rbp 0) rax)
-                  (set! rax (+ rax rax))
-                  (set! rax (+ rax (disp rbp 0)))
-                  (r15))])
-  (set! rax 17)
-  (f$1))
+(program ([f$1 (code
+                (set! rax (+ rax rax))
+                (r15))])
+         (set! rax 17)
+         (f$1))
 =>
-(code
+(program
  (set! rax 17)
  (jump f$1)
  f$1
- (set! (disp rbp 0) rax)
  (set! rax (+ rax rax))
- (set! rax (+ rax (disp rbp 0)))
  (jump r15))
 |#
 
 (define flatten-program
   (lambda (p)
     (define flatten
-      (lambda (p next-l)
-        (match p
-          [(program ,[flatten-defs -> def*] ,[tail])
-           (let ([tail (cond
-                        [(null? def*) tail]
-                        [else
-                         (match tail
-                           [(,st* ... (jump ,tail)) (guard (eq? tail (caar def*)))
-                            `(,st* ...)]
-                           [,tail tail])])])
-             `(program ,@tail ,def* ... ...))]
-          [(,label* (code () ,[tail*])) `(,label* ,@tail*)]
-          [(begin ,[ef*] ... ,[tail]) `(,ef* ... ... ,@tail)]
-          [(if ,test (,conseq) (,alt))
-           (cond [(eq? conseq next-l)
-                  `((if (not ,test) (jump ,alt)))]
-                 [(eq? alt next-l)
-                  `((if ,test (jump ,conseq)))]
-                 [else `((if ,test (jump ,conseq)) (jump ,alt))])]
-          [(set! ,a ,b) `((set! ,a ,b))]
-          [(mset! ,base ,off ,val) `((mset! ,base ,off ,val))]
-          [(,[triv]) (if (eq? triv next-l) '() `((jump ,triv)))]
-          [,p p])))
-    (define flatten-defs
-      (lambda (defs)
-        (match defs
-               [() '()]
-               [([,lab (code () ,body)]) `(,(flatten `(,lab (code () ,body)) #f))])))
-    (flatten p #f)))
-
-(define flatten-program
-  (lambda (p)
-    (define flatten
-      (lambda (p next-l)
-        (match p
-          [(letrec ,[flatten-defs -> def*] ,[tail])
-           (let ([tail (cond
-                        [(null? def*) tail]
-                        [else
-                         (match tail
-                           [(,st* ... (jump ,tail)) (guard (eq? tail (caar def*)))
-                            `(,st* ...)]
-                           [,tail tail])])])
-             `(code ,@tail ,def* ... ...))]
-          [(,label* (lambda () ,[tail*])) `(,label* ,@tail*)]
-          [(begin ,[ef*] ... ,[tail]) `(,ef* ... ... ,@tail)]
-          [(if ,test (,conseq) (,alt))
-           (cond [(eq? conseq next-l)
-                  `((if (not ,test) (jump ,alt)))]
-                 [(eq? alt next-l)
-                  `((if ,test (jump ,conseq)))]
-                 [else `((if ,test (jump ,conseq)) (jump ,alt))])]
-          [(set! ,a ,b) `((set! ,a ,b))]
-          [(mset! ,base ,off ,val) `((mset! ,base ,off ,val))]
-          [(,[triv]) (if (eq? triv next-l) '() `((jump ,triv)))]
-          [,p p])))
-    (define flatten-defs
-      (lambda (defs)
-        (match defs
-          [() '()]
-          [([,lab (lambda () ,body)]) `(,(flatten `(,lab (lambda () ,body)) #f))]
-          [([,lab1 (lambda () ,body1)]
-            [,lab2 (lambda () ,body2)]
-            [,lab3 (lambda () ,body3)] ...)
-           `(,(flatten `(,lab1 (lambda () ,body1)) lab2)
-             ,@(flatten-defs `([,lab2 (lambda () ,body2)]
-                               [,lab3 (lambda () ,body3)]...)))])))
-    (flatten p #f)))
+      (lambda (x)
+        (match x
+               [(if ,t (,c) (,a))
+                `((if ,t (jump ,c)) (jump ,a))]
+               [(begin ,[ef*] ... ,[tail]) `(,ef* ... ... ,@tail)]
+               [(set! ,a ,b) `((set! ,a ,b))]
+               [(,triv) `((jump ,triv))]
+               [,p p])))
+    (define program
+      (lambda (e)
+        (match e
+               [(program ([,label* (code ,[flatten -> body*] ...)] ...)
+                         ,[flatten -> tail*] ...)
+                (let* ([build (lambda (label body) (cons label (apply append body)))]
+                       [defs (map build label* body*)])
+                  `(program ,tail* ... ... ,defs ... ...))])))
+    (program p)))
 
 #!eof
 
 (flatten-program
- '(letrec ()
-    (begin
-      (set! rcx r15)
-      (set! rax 24)
-      (set! rax (+ rax 40))
-      (set! rax rax)
-      (rp.25))))
+ '(program ([f$1 (code
+                  (set! rax (+ rax rax))
+                  (r15))])
+           (set! rax 17)
+           (f$1)))
+
+(flatten-program
+ '(program ([f$1 (code
+                  (begin
+                    (set! rax (+ rax rax))
+                    (set! rax 17))
+                  (r15))])
+           (set! rax 17)
+           (f$1)))
