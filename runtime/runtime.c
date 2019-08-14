d207e08c1687eea8dccb02a5ad7b3724940985d6
tiancaiamao <tiancaiamao@gmail.com>
2019-08-15 00:21:17 +0800

    forget to add file

---
+++ runtime/runtime.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "types.h"
+
+void trampoline(struct VM* m) {
+  while (m->pc != NULL) {
+    m->pc(m);
+  }
+}
+
+Obj PrimEqual(Obj a, Obj b) {
+  if (a == b) {
+    return True;
+  }
+  return False;
+}
+
+void clofun17042(struct VM* m) {
+	Obj reg19610 = m->stack[0];
+	Obj reg19614 = m->stack[1];
+	Obj reg19705 = closureRef(reg19610, 0);
+	m->pc = closureFn(reg19705);
+	Obj reg19757 = closureRef(reg19610, 0);
+	Obj reg19827 = closureRef(reg19610, 1);
+	Obj reg19852 = (reg19827 * reg19614) >> 1;
+	m->stack[0] = reg19757;
+	m->stack[1] = reg19852;
+	return;
+}
+
+void clofun16875(struct VM* m) {
+	Obj reg18959 = m->stack[0];
+	Obj reg18963 = m->stack[1];
+	Obj reg18967 = m->stack[2];
+	Obj reg19052 = 0;
+	Obj reg19053 = PrimEqual(reg18967, reg19052);
+	if (reg19053 == True) {
+		m->pc = closureFn(reg18963);
+		Obj reg19242 = 2;
+		m->stack[0] = reg18963;
+		m->stack[1] = reg19242;
+		return;
+	} else {
+		Obj reg19343 = 2;
+		Obj reg19344 = reg18967 - reg19343;
+    Obj reg19413 = GetSymbolValue(intern("fact"));
+		m->pc = closureFn(reg19413);
+		Obj reg19435 = GetSymbolValue(intern("fact"));
+		Obj reg19544 = makeClosure(clofun17042, 2, reg18963, reg18967);
+		m->stack[0] = reg19435;
+		m->stack[1] = reg19544;
+		m->stack[2] = reg19344;
+		return;
+	}
+}
+
+void clofun16732(struct VM* m) {
+	Obj reg19887 = m->stack[0];
+	Obj reg19891 = m->stack[1];
+	m->stack[0] = reg19891;
+  m->pc = NULL;
+	return;
+}
+
+void clofun17313(struct VM* m) {
+	Obj reg18689 = makeClosure(clofun16875, 0);
+	Obj reg18758 = intern("fact");
+	Obj reg18783 = funSet(reg18758, reg18689);
+  Obj reg18852 = GetSymbolValue(intern("fact"));
+	m->pc = closureFn(reg18852);
+	Obj reg18874 = GetSymbolValue(intern("fact"));
+	Obj reg18899 = makeClosure(clofun16732, 0);
+	Obj reg18921 = 10;
+	m->stack[0] = reg18874;
+	m->stack[1] = reg18899;
+	m->stack[2] = reg18921;
+	return;
+}
+
+
+int main(int argc, char *argv[]) {
+  struct VM* m = newVM();
+	m->pc = clofun17313;
+  trampoline(m);
+  printf("... %ld\n", m->stack[0]);
+}
---
+++ runtime/types.h
@@ -0,0 +1,66 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#include <stdint.h>
+
+typedef uintptr_t Obj;
+
+// 000 fixnum
+// 001 non-fixnum
+// 001 symbol
+// 011 cons
+// 101 immediate const (boolean, null, undef...)
+// 111 general pointer (string, vector, number, error...)
+#define TAG_FIXNUM 0x0
+#define TAG_SYMBOL 0x1
+#define TAG_CONS 0x3
+#define TAG_IMMEDIATE_CONST 0x5
+#define TAG_PTR 0x7
+
+#define TAG_SHIFT 3
+#define TAG_MASK 0x7
+
+// 1-101 boolean
+// 0-101 other constant
+#define TAG_BOOLEAN 0xd
+
+#define tag(x) ((x) & TAG_MASK)
+#define iscons(x) (tag(x) == TAG_CONS)
+#define issymbol(x) (tag(x) == TAG_SYMBOL)
+#define isfixnum(x) (((x) & 1) == 0)
+#define isboolean(x) (((x) & 0xf) == TAG_BOOLEAN)
+
+const Obj True;
+const Obj False;
+const Obj Nil;
+const Obj Undef;
+
+struct VM;
+typedef void(*ClosureFn)(struct VM*);
+
+struct VM {
+  Obj* stack;
+  int size;
+  int idx;
+  ClosureFn pc;
+};
+
+
+struct VM* newVM();
+
+#define intern(x) makeSymbol(x)
+Obj makeSymbol(char *s);
+Obj GetSymbolValue(Obj sym);
+Obj funSet(Obj sym, Obj val);
+
+Obj makeClosure(ClosureFn fn, int count, ...);
+Obj closureRef(Obj o, int idx);
+ClosureFn closureFn(Obj o);
+
+#define ptr(x) ((void*)((x)&~TAG_PTR))
+#define fixnum(x) (((uint)(x))>>1)
+#define car(v) (((struct scmCons*)(ptr(v)))->car)
+#define cdr(v) (((struct scmCons*)(ptr(v)))->cdr)
+#define cons(x,y) makeCons(x,y)
+
+#endif
