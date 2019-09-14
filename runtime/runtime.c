0b2ce181f5fe90990b6980f7e051a6f528b5fe52
tiancaiamao <tiancaiamao@gmail.com>
2019-09-14 11:54:19 +0800

    meet a bug 'and' is not handled in cps-convert

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -20,67 +20,23 @@ Obj primNumberMultiply(Obj x, Obj y) {
 }
 
 
-static void clofun20861 (struct VM* m);
-static void clofun20773 (struct VM* m);
-static void clofun20495 (struct VM* m);
-
-static void
-clofun20861 (struct VM* m) {
-  Obj reg20925 = makeClosure(clofun20773, 0);
-  Obj reg20994 = 6;
-  Obj reg21013 = Nil;
-  Obj reg21014 = cons(reg20994, reg21013);
-  Obj reg21083 = 4;
-  Obj reg21114 = cons(reg21083, reg21014);
-  Obj reg21183 = 2;
-  Obj reg21214 = cons(reg21183, reg21114);
-  Obj reg21283 = symbolGet(intern("map"));
-  m->pc = closureFn(reg21283);
-  Obj reg21305 = symbolGet(intern("map"));
-  Obj reg21330 = makeClosure(clofun20495, 0);
-  m->stack[0] = reg21305;
-  m->stack[1] = reg21330;
-  m->stack[2] = reg20925;
-  m->stack[3] = reg21214;
-  return;
-}
-static void
-clofun20773 (struct VM* m) {
-  Obj reg21443 = m->stack[0];
-  Obj reg21447 = m->stack[1];
-  Obj reg21451 = m->stack[2];
-  m->pc = closureFn(reg21447);
-  Obj reg21620 = 2;
-  Obj reg21621 = reg21451 + reg21620;
-  m->stack[0] = reg21447;
-  m->stack[1] = reg21621;
-  return;
-}
-static void
-clofun20495 (struct VM* m) {
-  Obj reg21656 = m->stack[0];
-  Obj reg21660 = m->stack[1];
-  m->stack[0] = reg21660;
-  m->pc = NULL;
-  return;
-}
-
-
-void clofun_map (struct VM* m);
+void clofun_recur (struct VM* m);
+void clo_xxx (struct VM* m);
 
 int main(int argc, char *argv[]) {
   struct VM* m = newVM();
   /* init(); */
-	m->pc = clofun_map ;
+	m->pc = clofun_recur ;
   trampoline(m);
   Obj res = m->stack[0];
 
-  m->pc = clofun20861;
+  m->pc = clo_xxx;
   trampoline(m);
   res = m->stack[0];
 
-  printf("... %ld\n", car(res));
-  printf("... %ld\n", car(cdr(res)));
-  printf("... %ld\n", car(cdr(cdr(res))));
+  printf("... %ld\n", res);
+  /* printf("... %ld\n", car(res)); */
+  /* printf("... %ld\n", car(cdr(res))); */
+  /* printf("... %ld\n", car(cdr(cdr(res)))); */
   /* printf("... %ld\n", car(cdr(cdr(cdr(res))))); */
 }
--- test/recur-func-macro.scm
+++ test/recur-func-macro.scm
@@ -1,17 +1,1 @@
-;; The macroexpand result of recur
-;; (func recur
-;;       res 0 => res
-;;       res n => (recur (+ res 1) (- n 1)))
-
-(set (quote recur)
-     (lambda (#p193 #p194)
-       ((lambda (#cc195)
-          (if (and (cons? (cons #p193 (cons #p194 ()))) (not (null? (cons #p193 (cons #p194 ())))))
-              ((lambda (res)
-                 (if (and (cons? (cdr (cons #p193 (cons #p194 ())))) (not (null? (cdr (cons #p193 (cons #p194 ()))))))
-                     (if (equal? 0 (car (cdr (cons #p193 (cons #p194 ())))))
-                         (if (null? (cdr (cdr (cons #p193 (cons #p194 ())))))
-                             res
-                             (#cc195))
-                         (#cc195))
-                     (#cc195))) (car (cons #p193 (cons #p194 ())))) (#cc195))) (lambda () ((lambda (#cc196) (if (and (cons? (cons #p193 (cons #p194 ()))) (not (null? (cons #p193 (cons #p194 ()))))) ((lambda (res) (if (and (cons? (cdr (cons #p193 (cons #p194 ())))) (not (null? (cdr (cons #p193 (cons #p194 ())))))) ((lambda (n) (if (null? (cdr (cdr (cons #p193 (cons #p194 ()))))) (recur (+ res 1) (- n 1)) (#cc196))) (car (cdr (cons #p193 (cons #p194 ()))))) (#cc196))) (car (cons #p193 (cons #p194 ())))) (#cc196))) (lambda () (error "no match-help found!")))))))
+(set (quote recur) (lambda (#p53 #p54) ((lambda (#cc55) (if (= 0 #p54) #p53 (#cc55))) (lambda () ((lambda (#cc56) (recur (+ #p53 1) (- #p54 1))) (lambda () (error "no match-help found!")))))))
