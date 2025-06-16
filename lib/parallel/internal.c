581cfdef3b6674f84acd2a222d523ddb8d178231
tiancaiamao <tiancaiamao@gmail.com>
2025-06-16 14:44:45 +0800

    add preliminary support for parallel

--- lib/parallel/internal.c
+++ lib/parallel/internal.c
@@ -11,25 +11,31 @@ static void*
 startRoutine(void* arg) {
 	uintptr_t dummy;
 	struct Cora *co = coraInit(&dummy);
-	struct scmNative *ptr = (struct scmNative *)arg;
-	// TODO: memory of arg belongs to another thread, unsafe!
-	co->nargs = 1;
-	co->args[0] = ((Obj) ptr | TAG_PTR);
+	strBuf path1 = arg;
+	str path = toStr(path1);
+	Obj filePath = makeString(path.str, path.len);
+	strFree(path1);
+	co->nargs = 2;
+	/* co->args[0] = globalRef(intern("cora/lib/parallel#parallel-entry")); */
+	co->args[0] = globalRef(intern("load"));
+	co->args[1] = filePath;
 	trampoline(co, 0, coraDispatch);
+	printf("newProc exit\n");
 }
 
 static void
 newProc(struct Cora *co) {
-  Obj arg = co->args[1];
-  struct scmNative* proc = mustNative(arg);
-  pthread_t thread;
-  int ret = pthread_create(&thread, NULL, startRoutine, proc);
-  if (ret != 0) {
-	  coraReturn(co, Nil);
-	  return;
-  }
-  coraReturn(co, 42);
-  return;
+	Obj arg = co->args[1];
+	str path1 = stringStr(arg);
+	strBuf path2 = strDup(path1);
+	pthread_t thread;
+	int ret = pthread_create(&thread, NULL, startRoutine, path2);
+	if (ret != 0) {
+		coraReturn(co, Nil);
+		return;
+	}
+	coraReturn(co, 42);
+	return;
 }
 
 static void
@@ -223,38 +229,79 @@ mailboxRecvTry(struct Cora *co) {
 	coraReturn(co, v);
 }
 
-/* static void */
-/* mailboxRecvBlock(struct Cora *co) { */
-/* 	struct mailbox *m = mustC(co->args[1]); */
-/* 	Obj k = co->args[2]; */
-/* 	Obj wrap = co->args[3]; */
-/* 	int fd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC); */
+struct mailboxRegistrySlot {
+	struct mailbox *m;
+	strBuf name;
+};
 
-/* 	pthread_mutex_lock(&m->mu); */
-/* 	if (queueLen(m->data) > 0) { */
-/* 		// it may happen that there is no msg when try */
-/* 		// but when block, the msg is available... so double check here. */
-/* 		struct nodeData *n = dequeueData(m->data); */
-/* 		Obj v = n->v; */
-/* 		pthread_mutex_unlock(&m->mu); */
-/* 		coraReturn(co, v); */
-/* 		return; */
-/* 	} */
-	
-/* 	/\* struct nodeBlocked *n = malloc(sizeof(struct nodeBlocked)); *\/ */
-/* 	/\* n->fd = fd; *\/ */
-/* 	/\* n->k = k; *\/ */
-/* 	/\* n->wrap = wrap; *\/ */
 
-/* 	// add self to event loop so others can awake us. */
-/* 	// and also enqueue to mailbox blocked queue so send to mailbox trigger the awaking. */
-/* 	/\* pollReadAddPtr(n); *\/ */
-/* 	/\* enqueueBlocked(&m->blocked, n); *\/ */
+#define NUM_SLOTS 256
 
-/* 	/\* pthread_mutex_unlock(&m->mu); *\/ */
-/* 	coraReturn(co, False); */
-/* 	return; */
-/* } */
+struct mailboxRegistry {
+	struct mailboxRegistrySlot slots[NUM_SLOTS];
+};
+
+
+static unsigned long
+fnv1aHash(const char *str) {
+    unsigned long hash = 2166136261u;  // FNV offset basis
+    while (*str)
+        hash = (hash ^ (unsigned char)(*str++)) * 16777619;
+    return hash;
+}
+
+static struct mailboxRegistry registry;
+
+static void
+mailboxRegistryInit(struct mailboxRegistry *registry) {
+	memset(registry->slots, 0, NUM_SLOTS * sizeof(struct mailboxRegistry));
+}
+
+static void
+mailboxPublish(struct Cora *co) {
+	Obj arg1 = co->args[1];
+	Obj m = co->args[2];
+	str name = stringStr(arg1);
+	int idx = fnv1aHash(name.str) % NUM_SLOTS;
+	struct mailboxRegistrySlot *slot = &registry.slots[idx];
+	int start = (idx + NUM_SLOTS - 1) % NUM_SLOTS;
+	while(slot->m != NULL && idx != start) {
+		idx = (idx + 1) % NUM_SLOTS;
+		slot = &registry.slots[idx];
+	}
+	if (idx == start) {
+		printf("fuck, hash full!\n");
+		assert(false);
+		coraReturn(co, False);
+		return;
+	}
+	slot->m = mustCObj(m);
+	slot->name = strDup(name);
+	coraReturn(co, True);
+}
+
+static void
+mailboxResolve(struct Cora *co) {
+	Obj arg1 = co->args[1];
+	str name = stringStr(arg1);
+	int idx = fnv1aHash(name.str) % NUM_SLOTS;
+	struct mailboxRegistrySlot *slot = &registry.slots[idx];
+	// locate the slot
+	int try = 0;
+	while(slot->m != NULL && strCmp(name, toStr(slot->name)) != 0 && try < NUM_SLOTS) {
+		idx = (idx + 1) % NUM_SLOTS;
+		slot = &registry.slots[idx];
+		try++;
+	}
+	if (slot->m == NULL || try == NUM_SLOTS) {
+		// not found
+		coraReturn(co, Nil);
+		return;
+	}
+	assert(strCmp(name, toStr(slot->name)) == 0);
+	coraReturn(co, makeCObj(slot->m));
+	return;
+}
 
 static struct registerModule parallelModule = {
   NULL,
@@ -269,6 +316,8 @@ static struct registerModule parallelModule = {
     {"mailbox-queue-len", mailboxQueueLen, 1},
     {"mailbox-dequeue", mailboxDequeue, 1},
     {"mailbox-enqueue-blocked", mailboxEnqueueBlocked, 2},
+    {"mailbox-publish", mailboxPublish, 2},
+    {"mailbox-resolve", mailboxResolve, 1},
     {NULL, NULL, 0},
   }
 };
