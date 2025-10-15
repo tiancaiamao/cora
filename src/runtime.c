90a3ba84b2d852cd215f9e8dd05511388d3984ff
tiancaiamao <tiancaiamao@gmail.com>
2025-10-15 15:40:51 +0800

    change coraCall API to avoid using va_list

--- src/runtime.c
+++ src/runtime.c
@@ -91,14 +91,14 @@ coraRun(struct Cora *co) {
 	trampoline(co);
 }
 
-static void coraDispatch(struct Cora *co, Obj fn, int nargs, va_list ap);
+static void coraDispatch(struct Cora *co, Obj fn, int nargs, Obj* args);
 
-static void
-coraCallv(struct Cora *co, Obj fn, int nargs, va_list ap) {
+void
+coraCall(struct Cora *co, Obj fn, int nargs, Obj *args) {
 	struct scmNative* f = mustNative(fn);
 	if (f->required != nargs) {
 		// curry or partial, goto dispatch function
-		coraDispatch(co, fn, nargs, ap);
+		coraDispatch(co, fn, nargs, args);
 		return;
 	}
 	int nframe = f->nframe;
@@ -109,21 +109,13 @@ coraCallv(struct Cora *co, Obj fn, int nargs, va_list ap) {
 	frame[0] = fn;
 	if (nargs > 0) {
 		for (int i = 1; i <= nargs; i++) {
-			frame[i] = va_arg(ap, Obj);
+			frame[i] = args[i-1];
 		}
 	}
 	// set the new ctx
 	co->ctx.fn = f->fn;
 	co->ctx.label = 0;
 	return;
-}
-
-void
-coraCall(struct Cora *co, Obj fn, int nargs, ...) {
-	va_list ap;
-	va_start(ap, nargs);
-	coraCallv(co, fn, nargs, ap);
-	va_end(ap);
 }
 
 static void
@@ -139,7 +131,7 @@ callCurry(struct Cora *co, int label, Obj *R) {
 }
 
 Obj
-makeCurry(Obj fn, int nargs, va_list ap) {
+makeCurry(Obj fn, int nargs, Obj *args) {
 	int sz = sizeof(struct scmNative) + (nargs + 1) * sizeof(Obj);
 	struct scmNative *clo = newObj(scmHeadNative, sz);
 	clo->fn = callCurry;
@@ -151,20 +143,20 @@ makeCurry(Obj fn, int nargs, va_list ap) {
 	// fn + args
 	clo->data[0] = fn;
 	for (int i = 0; i < nargs; i++) {
-		clo->data[i+1] = va_arg(ap, Obj);
+		clo->data[i+1] = args[i];
 	}
 	return ((Obj) (&clo->head) | TAG_PTR);
 }
 
 static void
-coraDispatch(struct Cora *co, Obj fn, int nargs, va_list ap) {
+coraDispatch(struct Cora *co, Obj fn, int nargs, Obj* args) {
 	struct scmNative *f = mustNative(fn);
 	int required = f->required;
 	if (nargs == required) {
 		// TODO?
 		assert(false);
 	} else if (nargs < required) {
-		Obj ret = makeCurry(fn, nargs, ap);
+		Obj ret = makeCurry(fn, nargs, args);
 		coraReturn(co, ret);
 	} else {
 		// eval the first call and get the result;
@@ -177,14 +169,14 @@ coraDispatch(struct Cora *co, Obj fn, int nargs, va_list ap) {
 		Obj *R = stackAlloc(co, f->nframe);
 		R[0] = fn;
 		for (int i=0; i<required; i++) {
-			R[i+1] = va_arg(ap, Obj);
+			R[i+1] = args[i];
 		}
 		co->ctx.fn = f->fn;
 		co->ctx.label = 0;
 		trampoline(co);
 
 		// make the next call.
-		coraCallv(co, co->res, nargs - required, ap);
+		coraCall(co, co->res, nargs - required, args + required);
 	}
 	return;
 }
@@ -543,14 +535,7 @@ builtinImport(struct Cora *co, int label, Obj *R) {
 	str tmp1 = toStr(tmp);
 
 	coraCall2(co, makeNative(3, builtinLoad, 2, 0), makeString(tmp1.str, tmp1.len), pkg);
-
-	/* co->nargs = 3; */
-	/* co->args[0] = makeNative(0, builtinLoad, 2, 0); */
-	/* co->args[1] = makeString(tmp1.str, tmp1.len); */
-	/* co->args[2] = pkg; */
-	/* trampoline(co, 0, coraDispatch); */
 	strFree(tmp);
-	/* coraReturn(co, pkg); */
 }
 
 static void
@@ -956,10 +941,6 @@ applyClosureForEval(struct Cora *co, int label, Obj *R) {
 	}
 	co->ctx.sp = R;
 	coraCall2(co, globalRef(intern("cora/lib/eval#eval")), body, env);
-	/* R[0] = globalRef(intern("cora/lib/eval#eval")); */
-	/* R[1] = body; */
-	/* R[2] = env; */
-	/* co->ctx.pc.func = coraDispatch; */
 }
 
 static void
--- src/runtime.h
+++ src/runtime.h
@@ -65,7 +65,7 @@ coraReturn(struct Cora *co, Obj val) {
 	return;
 }
 
-void coraCall(struct Cora *co, Obj fn, int nargs, ...);
+void coraCall(struct Cora *co, Obj fn, int nargs, Obj *args);
 
 static inline void
 coraCall0(struct Cora *co, Obj fn) {
@@ -86,7 +86,8 @@ coraCall1(struct Cora *co, Obj fn, Obj arg1) {
 	struct scmNative *f = ptr(fn);
 	assert(f->head.type == scmHeadNative);
 	if (f->required != 1) {
-		return coraCall(co, fn, 1, arg1);
+		Obj args[1] = {arg1};
+		return coraCall(co, fn, 1, args);
 	}
 	Obj *frame = stackAlloc(co, f->nframe);
 	co->ctx.fn = f->fn;
@@ -100,7 +101,8 @@ coraCall2(struct Cora *co, Obj fn, Obj arg1, Obj arg2) {
 	struct scmNative *f = ptr(fn);
 	assert(f->head.type == scmHeadNative);
 	if (f->required != 2) {
-		return coraCall(co, fn, 2, arg1, arg2);
+		Obj args[2] = {arg1, arg2};
+		return coraCall(co, fn, 2, args);
 	}
 	Obj* frame = stackAlloc(co, f->nframe);
 	co->ctx.fn = f->fn;
@@ -115,7 +117,8 @@ coraCall3(struct Cora *co, Obj fn, Obj arg1, Obj arg2, Obj arg3) {
 	struct scmNative *f = ptr(fn);
 	assert(f->head.type == scmHeadNative);
 	if (f->required != 3) {
-		return coraCall(co, fn, 3, arg1, arg2, arg3);
+		Obj args[3] = {arg1, arg2, arg3};
+		return coraCall(co, fn, 3, args);
 	}
 	Obj *frame = stackAlloc(co, f->nframe);
 	co->ctx.fn = f->fn;
@@ -135,7 +138,8 @@ coraCall4(struct Cora *co, Obj fn, Obj arg1, Obj arg2, Obj arg3, Obj arg4) {
 	struct scmNative *f = ptr(fn);
 	assert(f->head.type == scmHeadNative);
 	if (f->required != 4) {
-		return coraCall(co, fn, 4, arg1, arg2, arg3, arg4);
+		Obj args[4] = {arg1, arg2, arg3, arg4};
+		return coraCall(co, fn, 4, args);
 	}
 	Obj *frame = stackAlloc(co, f->nframe);
 	co->ctx.fn = f->fn;
