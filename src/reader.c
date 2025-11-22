068f48137ab4fd722539ac6e883ad0ef1d74c71a
tiancaiamao <tiancaiamao@gmail.com>
2025-11-23 02:27:20 +0800

    debug CI

--- src/reader.c
+++ src/reader.c
@@ -253,7 +253,7 @@ printCons(FILE *to, Obj o, bool start) {
 void
 printObj(FILE *to, Obj o) {
 	if (isfixnum(o)) {
-		fprintf(to, "%lld", fixnum(o));
+		fprintf(to, "%ld", fixnum(o));
 	} else if (iscobj(o)) {
 		fprintf(to, "cobj");
 	} else if (iscons(o)) {
